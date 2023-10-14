367be1dcc824dae1472152917c7363001597431a
mgalihpp <muhammadgalih451@gmail.com>
2023-10-14 22:19:39 +0700

    first commit

---
+++ variable.py
@@ -0,0 +1,34 @@
+########### VARIABLE ###########
+
+#string
+
+# first_name = "mgpp"
+# last_name = "Code"
+# full_name = first_name + " " + last_name
+# print("hello " + full_name)
+# # print("Hello", name)
+
+# integer
+
+# age = 18
+# # age += "lol" #type error
+
+# print("you age is: " + str(age)) #membuat int to string
+
+#float
+
+# height = 250.5
+# print("your height is: " + str(height) + "cm")
+# print(type(height))
+
+#boolean
+
+human = False
+name = 'galih'
+# print(human)
+# print(type(human))
+
+if(human == True):
+    print("hello" + name)
+else:
+    print("lu siapa")
